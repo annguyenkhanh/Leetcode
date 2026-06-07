82e562cc49f075ada95c0f91753474d4fbf85f42
AnNguyen <33521739+annguyenkhanh@users.noreply.github.com>
2026-06-07 09:41:15 +0700

    Time: 0 ms (100%), Space: 12.4 MB (93.23%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        auto max = std::max_element(candies.begin(), candies.end());
+        vector<bool> st;
+        for(char c : candies){
+            auto total = c + extraCandies;
+            if (total >= *max){
+                st.push_back(true);
+            }
+            else st.push_back(false);
+        }
+        return st;
+    }
+};
+
+//mindset:
+// tạo 1 giá trị max, và 1 vòng for từng Kid, + extra cho kid đó, nếu lớn hơn max thì true không thì false
